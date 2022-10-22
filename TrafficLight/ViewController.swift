c04a5f0f692114330a8084021f7c96bc2b4c4cd3
IgorPrivalikhin <iprivalihin@mail.ru>
2022-10-22 18:20:52 +0300

    ViewLogic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    
+    @IBOutlet var yellowLight: UIView!
+    
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var greetingButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.layer.cornerRadius = 55
+        redLight.alpha = 0.4
+        
+        yellowLight.layer.cornerRadius = 55
+        yellowLight.alpha = 0.4
+        
+        greenLight.layer.cornerRadius = 55
+        greenLight.alpha = 0.4
+        
+        greetingButton.layer.cornerRadius = 10
+        
+    
     }
 
 
+    @IBAction func greetingButtonTapped() {
+        greetingButton.setTitle("NEXT", for: .normal)
+        yellowLight.alpha = 1
+        
+    }
+    
+    
+    
+    
 }
 
